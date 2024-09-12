9687aad228fd65608f5df21667fab8fc4bad5f4d
jolahmilovski@gmail.com <jolahmilovski@gmail.com>
2024-09-12 12:53:05 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿
+namespace c_SimpleCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, secondValue;
+            Console.WriteLine("Choose operation: '+','-','*','/'");
+            string action = Console.ReadLine();
+            Console.WriteLine("Input num 1:");
+            firstValue = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input num 2:");
+            secondValue = double.Parse(Console.ReadLine());
+
+            //switch (action)
+            //{
+            //    case "+": 
+            //        Console.WriteLine(firstValue + secondValue);
+            //    break;
+            //    case "-":
+            //        Console.WriteLine(firstValue - secondValue); 
+            //    break;
+            //    case "*":
+            //        Console.WriteLine(firstValue * secondValue);
+            //    break;
+            //    case "/":
+            //        Console.WriteLine(firstValue / secondValue);
+            //    break;
+                
+            //    default:
+            //        Console.WriteLine("Error!!");
+            //    break;
+            //}
+            if (action == "+")
+            {
+                Console.WriteLine(firstValue + secondValue);
+            }
+            else if (action == "-")
+            {
+                  Console.WriteLine(firstValue - secondValue);
+            }
+            else if (action == "*")
+            {
+                Console.WriteLine(firstValue * secondValue);
+            }
+            else if(action == "/")
+            {
+                Console.WriteLine(firstValue / secondValue);
+            }
+            else { Console.WriteLine("ERROR!!!"); }
+        }
+
+    }       
+}
